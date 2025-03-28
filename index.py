f42343cf971ffa9eb5c1c9d6441594ff38423900
Shadow <alioubafou@gmail.com>
2025-03-28 16:50:11 +0100

    Correctif de l'interface grahique

--- index.py
+++ index.py
@@ -18,12 +18,11 @@ choixImage = random.choice(listImages)
 imageInitial = ImageTk.PhotoImage(choixImage.resize((350, 350)))
 
 titre = Label(fenetre, text="Choisi un nombre pour jouer", 
-              font=("Arial", 12, "bold"), fg="white", 
-              width=30, bg="black")
-titre.place(relx=0.5, y=50, anchor="center", height=40)
+              font=("Orbitron", 12, "bold"), bg="black", fg="white")
+titre.place(relx=0.5, y=50, anchor="center", height=40, width=300)
 
 champ = Entry(fenetre)
-champ.place(relx=0.5, y=120, anchor="center", height=30, width=50)
+champ.place(relx=0.5, y=120, anchor="center", height=30, width=200 )
 
 label0 = Label(fenetre, bg="black")
 label0.place(relx=0.5, y=573, anchor="center", height=50, width=400)
@@ -34,9 +33,9 @@ def verifie_valeur(x):
         if 1 <= x <= 6:
             return x
         else:
-            label0.config(text="Veuillez choisir un nombre entre 1 et 6")
+            label0.config(text="Veuillez choisir un nombre entre 1 et 6", fg="#ff0000", font=("Consolas", 14, "bold"))
     else:
-        label0.config(text="Saisie invalide, entrez un nombre entre 1 et 6")
+        label0.config(text="Saisie invalide, entrez un nombre entre 1 et 6", fg="#ff0000", font=("Consolas", 14, "bold"))
     return None
 
 # Cette fonction met à jour l'image du label
@@ -46,18 +45,15 @@ def lancer():
     indexImage = listImages.index(de_lance)
     imageAffiche = ImageTk.PhotoImage(de_lance.resize((350, 350)))
     
-    # Mettre à jour l'image du label
     label.config(image=imageAffiche)
     
-    # Conserver la référence de l'image dans la fenêtre pour éviter que l'image soit effacée
     label.image = imageAffiche
 
-    if (nombre - 1) == indexImage:
-        label0.config(text="Bravo, vous avez gagné !")
+    if nombre - 1 == indexImage:
+        label0.config(text="Bravo, vous avez gagné !", fg="white")
     else:
         label0.config(text="Dommage vous avez perdu ! Résessayer", fg="white")
 
-# Créer le label avec l'image initiale
 label = Label(fenetre, image=imageInitial)
 label.place(relx=0.5, y=330, anchor="center", width=350, height=350)
 
